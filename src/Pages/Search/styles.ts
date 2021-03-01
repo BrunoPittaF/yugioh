01b247d829cf9946e296b96068b349285c230c3f
Caio Bruno <caiobrunopittaf@gmail.com>
2021-02-28 22:31:11 -0300

    pequenos ajustes na visualização e leitura do texto

--- src/Pages/Search/styles.ts
+++ src/Pages/Search/styles.ts
@@ -1,92 +1,92 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    h1{
-        color: #fff;
-        text-align: center;
-        margin: 20px 0px;
-    }
+  h1 {
+    color: #fff;
+    text-align: center;
+    margin: 20px 0px;
+  }
 
-    form{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 20px 0px;
-        input{
-            width: 300px;
-            padding: 5px;
-            border-radius: 4px;
-            outline: none;
-            border: none;
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0px;
+    input {
+      width: 300px;
+      padding: 5px;
+      border-radius: 4px;
+      outline: none;
+      border: none;
+    }
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px 20px;
+    figure {
+      position: relative;
+      &:active {
+        > figcaption {
+          transform: scale3d(1, 1, 1);
+          visibility: visible;
+          background-color: rgba(0, 0, 0, 1);
+          opacity: 0.8;
+          padding: 20px;
         }
-    }
-
-    .grid{
-        display: grid;
-        grid-template-columns: repeat(3,1fr);
-        figure{
-            position: relative;
-            &:active{
-                > figcaption{
-                    transform: scale3d(1, 1, 1);
-                    visibility: visible;
-                    background-color: rgba(0, 0, 0, 1);
-                    opacity: 0.8;
-                }
-            }
-            &:focus{
-                > figcaption{
-                    transform: scale3d(1, 1, 1);
-                    visibility: visible;
-                    background-color: rgba(0, 0, 0, 1);
-                    opacity: 0.8;
-                }
-            }
-            &:hover{
-                > figcaption{
-                    transform: scale3d(1, 1, 1);
-                    visibility: visible;
-                    background-color: rgba(0, 0, 0, 1);
-                    opacity: 0.8;
-                }
-            }
-            figcaption{
-                position: absolute;
-                top: 0;
-                bottom: 0;
-                left: 0;
-                right: 0;
-                width: 100%;
-                height: 100%;
-                background: #000;
-                opacity: 0;
-                visibility: hidden;
-                transform-origin: top center;
-                transform: scale3d(1,0,1);
-                transition: 1s;
-                p{
-                    color: white;
-                    font-size: 16px;
-                }
-
-
-            }
+      }
+      &:focus {
+        > figcaption {
+          transform: scale3d(1, 1, 1);
+          visibility: visible;
+          background-color: rgba(0, 0, 0, 1);
+          opacity: 0.8;
+          padding: 20px;
         }
-
-    }
-
-
-    @media (max-width: 475px){
-        .grid{
-            grid-template-columns: 1fr;
-            grid-gap: 20px 0px;
-            figure{
-                picture{
-                    img{
-                        width: 100%;
-                    }
-                }
-            }
+      }
+      &:hover {
+        > figcaption {
+          transform: scale3d(1, 1, 1);
+          visibility: visible;
+          background-color: rgba(0, 0, 0, 1);
+          opacity: 0.8;
+          padding: 20px;
         }
+      }
+      figcaption {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        background: #000;
+        opacity: 0;
+        visibility: hidden;
+        transform-origin: top center;
+        transform: scale3d(1, 0, 1);
+        transition: 1s;
+        p {
+          color: white;
+          font-size: 16px;
+        }
+      }
     }
+  }
+
+  @media (max-width: 475px) {
+    .grid {
+      grid-template-columns: 1fr;
+      grid-gap: 20px 0px;
+      figure {
+        picture {
+          img {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
 `;
