33239e711cdc67ecc961640a73a883a6ffe1aac1
Caio Bruno <caiobrunopittaf@gmail.com>
2021-03-01 19:51:23 -0300

    projeto tomando forma

---
+++ src/Components/ArchetypeList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Select } from "./ArchetypeList";
+
+const ArchetypeList: React.FC = () => {
+  return (
+    <Select>
+      <option value="">
+        Aqui vai sair a lista de archetypes pro cara pesquisar
+      </option>
+    </Select>
+  );
+};
+
+export default ArchetypeList;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home';
-import Search from './Pages/Search';
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
 
 const Routes: React.FC = () => {
   return (
-  <BrowserRouter>
-  <Switch>
-    <Route path="/" exact component={ Home }/>
-    <Route path="/search" component={ Search }/>
-  </Switch>
-  </BrowserRouter>
-    );
-}
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/search" component={Search} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
